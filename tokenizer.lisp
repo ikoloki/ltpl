341aea76a697a3499baa1ab3ac1694870e306a41
Michael <michael@pop-os.localdomain>
2022-03-11 17:05:57 -0500

    tokenizes with errors

--- tokenizer.lisp
+++ tokenizer.lisp
@@ -8,26 +8,28 @@
   (literal)
   (symbol))
 
-(defparameter *test* "==100,1(.*(A-Z))[w:file.txt][p].")
+(defparameter *test* "==100,1(.*(A-Z))$0$10$0[w:file.txt]$foo[p]bar.")
 (defparameter *input-stream* (make-string-input-stream (subseq *test* 2)))
 
 (defun tokenize (src)
-  (let* ((directive (tokenize-directive src))p
-	 (tokens '(directive)))
+  (let* ((directive (tokenize-directive src))
+	 (tokens `(,directive)))
     (loop for i from 0
-	  for c = (read-char *input-stream*) do
+	  for c = (read-char *input-stream* nil)
+	  while c do
 	    (cond ((eq c #\[) (push (tokenize-action) tokens))
 		  ((eq c #\') (push (tokenize-escape) tokens))
 		  ((eq c #\() (push (tokenize-regex) tokens))
 		  ((eq c #\,) (push (tokenize-comma c) tokens))
-		  ((eq c #\$) (push (tokenize-object-ref) tokens))
-		  ((eq c #\:) (push (tokenize-seperator c) tokens))
+		  ((eq c #\$) (push (tokenize-object-ref c) tokens))
+		  ((eq c #\:) (push (tokenize-colon c) tokens))
 		  ((eq c #\.) (push (tokenize-terminator c) tokens))
 		  ((alphanumericp c) (push (tokenize-object-primitive c) tokens))
 		  (t (push (make-token :literal c :symbol 'unknown-token) tokens))))
-    (reverse tokens)))
+   tokens))
 
 (defun tokenize-regex ()
+  (unread-char #\( *input-stream*)
   (let ((regex (with-output-to-string (output-stream)
 		 (loop with paren-stack
 		       for c = (read-char *input-stream*) do
@@ -35,35 +37,41 @@
 			       ((eq c #\)) (pop paren-stack)))
 			 (write-char c output-stream)
 			 (when (eq paren-stack '())
-		   (unread-char c *input-stream*)
 			   (loop-finish))))))
-    (make-token :literal regex :symbol 'regex)))
-
+    (make-token :literal ;;(subseq regex 1 (- (length regex) 1))
+		regex :symbol 'regex)))
+									    
 (defun tokenize-action ()
   (let ((action (with-output-to-string (output-stream)
 		 (loop with []-stack
 		       for c = (read-char *input-stream*) do
 			 (cond ((eq c #\[) (push c []-stack))
-			       ((eq c #\])) (pop []-stack))
-			 (write-char c output-stream)
-			 (when (eq []-stack '())
-			   (unread-char c *input-stream*)
+			       ((eq c #\]) (pop []-stack)))
+			       
+			       (write-char c output-stream)
+			       (when (eq []-stack '())
+				 (unread-char c *input-stream*)
 			   (loop-finish))))))
-    (make-token :literal action :symbol 'regex)))
+	(make-token :literal action :symbol 'action)))
 
 (defun tokenize-object-ref (c)
-  "looks for a string with the pattern of $XXX | $XX$...$..$.. and breaks it up into 
-a token with the literal of (first (second (third)))"
-  (let ((tokens nil))
-    (when (eq #\$ c)
-      ;; I want to accumulate a list here
-      (setf tokens (tokenize-object-primitive (read-char *input-stream*)))
-      (return-from tokenize-object-ref (make-token (token-literal tokens) 'object-ref)))))
-  
+    "looks for a string with the pattern of $XXX | $XX$...$..$.. and breaks it up into 
+  a token with the literal of (first (second (third)))"
+  (unread-char c *input-stream*)
+  (let ((object (with-output-to-string (output-stream)
+		  (loop for c = (read-char *input-stream*)
+			if (or (alphanumericp c)
+			       (eq #\$ c)) do
+				 (write-char c output-stream)
+			else do 
+			  (unread-char c *input-stream*)
+			    (loop-finish)))))
+    (make-token :literal object :symbol 'unexpanded-object)))
+									    
 (defun tokenize-object-primitive (c)
-  "looks for a alphanumeric set of chars"
+"looks for a alphanumeric set of chars"
   (unread-char c *input-stream*) ;; unread c to prevent the first char from being cut off
-  (let ((object-primitive (with-output-to-string (output-stream) 
+  (let ((object-primitive (with-output-to-string (output-stream)
 			    (loop for c = (read-char *input-stream*)
 				  if (alphanumericp c) do
 				    (write-char c output-stream)
@@ -80,6 +88,9 @@ a token with the literal of (first (second (third)))"
 
 (defun tokenize-colon (c)
   (make-token :literal c :symbol 'seperator))
+
+(defun tokenize-terminator (c)
+  (make-token :literal c :symbol 'terminator))
    
 (defun tokenize-directive (string)
   (let ((directive (subseq string 0 (min 2 (length string))))

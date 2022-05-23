ad4bb8a46e0298c0b9351d635ad20ff323e6b68d
loki <michael@pop-os.localdomain>
2022-05-23 14:58:23 -0400

    Added more tokens, finished MOST of the parser, still have work todo

--- src/front end/tokenizer.lisp
+++ src/front end/tokenizer.lisp
@@ -4,28 +4,30 @@
   (literal)
   (symbol))
 
-(defparameter *test* "==100,1(.*(A-Z))$0$10![w:file.txt]$foo[p]$foo$[100+1]bar.")
+;; (defparameter *test* "==100,1(.*(A-Z))$0$10![w:file.txt]$foo[p]$foo$[100+1]bar.")
 
 (defun tokenize (input-stream)
   "takes tokens from the *input-stream* and processes them util the end"
-  ;; (setf src (remove-if (lambda (c) (or (eq #\SPACE c) (eq #\NEWLINE c))) src))
   (let* ((directive (tokenize-directive (with-output-to-string (tmp-stream)
                                           (loop for i from 0 to 1
                                                 for c = (read-char input-stream nil) do
-                                                (write-char c tmp-stream)))))
+                                                  (write-char c tmp-stream)))))
          (tokens `(,directive)))
     (loop :for c = (read-char input-stream nil)
           :while c do
             (cond ((eq c #\[) (push (tokenize-[ c) tokens))
                   ((eq c #\]) (push (tokenize-] c) tokens))
                   ((eq c #\:) (push (tokenize-colon c) tokens))
-                  ((eq c #\') (push (tokenize-escape c) tokens))
+                  ((eq c #\\) (push (tokenize-escape c) tokens))
                   ((eq c #\() (push (tokenize-regex input-stream) tokens))
                   ((eq c #\@) (push (tokenize-reader-end c) tokens))
                   ((eq c #\,) (push (tokenize-comma c) tokens))
                   ((eq c #\$) (push (tokenize-object input-stream c) tokens))
-                  ((eq c #\.) (push (tokenize-terminator c) tokens))
-                  ((operatorp c) (push (tokenize-operator c) tokens))
+                  ((eq c #\') (filter-comment input-stream c))
+                  ((eq c #\.) (push (tokenize-object input-stream c) tokens))
+                  ((eq c #\|) (push (tokenize-bar input-stream c) tokens))
+                  ((eq c #\*) (push (tokenize-star input-stream c) tokens))
+                  ((operatorp input-stream c) (push (tokenize-operator c) tokens))
                   ((alphanumericp c) (push (tokenize-object input-stream c) tokens))
                   (t (push (make-token :literal c :symbol 'unknown-token) tokens))))
     (reverse tokens)))
@@ -44,15 +46,19 @@
     (make-token :literal (subseq regex 1 (- (length regex) 1))
                 :symbol 'regex)))
 
-
 (defun tokenize-object (input-stream c)
-  "takes a object referance out of the stream denoted by foo or 10 10.0 foo.0"
-  (unread-char c input-stream) ;; unread c to prevent the first char from being cut off
-  (let* ((object (with-output-to-string (output-stream)
+  "takes a object reference out of the stream denoted by foo or 10 10.0 foo.0 .10 20."
+  (unread-char c input-stream) ;; unread c to prevent the first char from being cut off 
+  (let* ((floating-point nil)
+         (hashtag nil)
+         (object (with-output-to-string (output-stream)
                    (loop :for c := (read-char input-stream nil)
                          :for i from 0
                          :if (null c) :do
                            (loop-finish)
+                         :else :if (and (eq c #\.) (null floating-point)):do
+                           (write-char c output-stream)
+                           (setf floating-point t)
                          :else :if (alphanumericp c) :do
                            (write-char c output-stream)
                          :else :if (and (eq #\$ c) (= i 0)) :do
@@ -65,29 +71,46 @@
         (make-token :literal object :symbol 'object-primitive))))
 
 (defun tokenize-directive (directive)
-  (let ((mode nil))
-    (cond ((equal directive "==") (setf mode 'reader))
-	  ((equal directive "||") (setf mode 'reader))
-	  ((equal directive "--") (setf mode 'reader))
-	  ((equal directive "<=") (setf mode 'reader))
-	  ((equal directive "^^") (setf mode 'reader))
-    (t (setf mode 'reader-error)))
-    (make-token :literal directive :symbol mode)))
+  "looks for a specific way to parse a file"
+  (make-token :literal directive :symbol 'reader)
+
+(defun filter-comment (input-stream c)
+  "filters everything between ' and '"
+  (loop :for c := (read-char input-stream) :do
+    (when (eq #\' c)
+      (loop-finish))))
+
+(defun operatorp (input-stream c)
+  (let ((operators '(#\\ #\+ #\- #\% #\! #\~)))
+    (dolist (op operators)
+      (when (eq op c)
+        (return-from operatorp t)))))
+
+(defun tokenize-bar (input-stream c)
+  "determines if its a the split at or the split operator"
+  (when (and (eq c #\|) (eq (read-char input-stream) #\|))
+    (return-from tokenize-bar (make-token :literal "||" :symbol 'operator)))
+  (unread-char input-stream)
+  (make-token :literal #\| :symbol 'operator))
+
+(defun tokenize-star (input-stream c)
+  "determines if its a exponent or a multiplication"
+  (when (and (eq c #\*) (eq (read-char input-stream) #\*))
+    (return-from tokenize-star (make-token :literal "**" :symbol 'operator)))
+  (unread-char input-stream)
+  (make-token :literal #\* :symbol 'operator))
 
 (defun tokenize-operator (c)
   (make-token :literal c :symbol 'operator))
-
-(defun tokenize-terminator (c)
-  (make-token :literal c :symbol 'terminator))
 
 (defun tokenize-escape (c)
   (make-token :literal c :symbol 'esc))
 
 (defun tokenize-colon (c)
-  (make-token :literal c :symbol 'arg-seperator))
+  (make-token :literal c :symbol 'arg-delim))
 
 (defun tokenize-comma (c)
-  (make-token :literal c :symbol 'range-seperator))
+  (make-token :literal c :symbol 'range-delim))
 
 (defun tokenize-[ (c)
   (make-token :literal c :symbol 'action-begin))
@@ -95,12 +118,5 @@
 (defun tokenize-] (c)
   (make-token :literal c :symbol 'action-end))
 
-(defun token-reader-end (c)
-  (make-token :literal c :symbol 'reader))
-
-(defun operatorp (c)
-  (let ((operators '(#\\ #\+ #\- #\* #\/ #\| #\!)))
-    (dolist (op operators)
-      (when (eq op c)
-	(return-from operatorp t)))))
-
+(defun tokenize-reader-end (c)
+  (make-token :literal c :symbol 'reader-end))

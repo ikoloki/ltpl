8e324f2d743a609feaa77118cf91b9b81b9e827e
loki <michael@pop-os.localdomain>
2022-06-05 22:51:00 -0400

    Fixed errors handling, added to tokenizer

--- src/front end/errors.lisp
+++ src/front end/errors.lisp
@@ -1,3 +1,23 @@
 (in-package :ltpl)
 
-(defvar *parser-error-stream* (make-object-stream '()))
+(defstruct parser-error
+  (token)
+  (code)
+  (severity))
+
+(defparameter *parser-error-stream* (make-object-stream '()))
+
+(defun parser-report-return (parser-error ast)
+  (write-object *parser-error-stream* parser-error)
+  ast)
+
+(defun parser-report (parser-error input-stream ast)
+  (with-token input-stream
+    (write-object *parser-error-stream* parser-error)
+    (tokenize-unknown-token input-stream ast)))
+
+(defmacro parser-report-warning (token code input-stream ast)
+  `(parser-report (make-parser-error :token token :code code :severity 'warning) ,input-stream ,ast))
+
+(defmacro parser-report-error (token code input-stream ast)
+  `(parser-report (make-parser-error :token token :code code :severity 'error) ,input-stream ,ast))
